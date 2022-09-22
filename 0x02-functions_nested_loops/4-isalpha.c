fa154a278f2361f054784feec1b732bda4d03ab5
James Kamau <jn89382a@gmail.com>
2022-09-22 13:02:22 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_isalpha - function to check if c is a 
+ *_isalpha - function to check if c is a
  * letter, lowercase or uppercase
  *@c: is the int that will use for the
  *argument of the function
