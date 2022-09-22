0fd28afa4398988c16f66cbae1dfa4b0f95604fe
James Kamau <jn89382a@gmail.com>
2022-09-22 14:32:40 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *add - Sum 
+ *@num1: One of the integers to be summed up
+ *@num2: 2nd integer to be summed up
+ *Return: 0(Successful)
+ */
+
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
