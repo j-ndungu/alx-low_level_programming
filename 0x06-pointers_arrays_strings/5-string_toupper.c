13e90694fc26c5e5afcf852751529724dd0aa66b
James Kamau <jn89382a@gmail.com>
2022-09-23 11:13:45 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 
 /**
  * string_toupper - Changes all lowercase letters
- * of a string to uppe
+ * of a string to uppercase
  * @str: The string to be changed.
  * Return: A pointer to the changed string.
  */
@@ -14,7 +14,7 @@ while (str[index])
 {
 if (str[index] >= 'a' && str[index] <= 'z')
 str[index] -= 32;
-index++;      
+index++;
 }
 return (str);
 }
